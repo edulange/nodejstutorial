c5f21bca8de6a0a842a052b3d8d243c644f86103
Eduardo Lange <eduardoandrelange@gmail.com>
2024-03-16 19:11:34 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ const path = require('path')
 
 console.log(os.type())
 console.log(os.version())
-console.log(os.userInfo())
 
 console.log(os.homedir())
 console.log(__dirname)
 console.log(__filename)
 
-console.log(path.dirname(__dirname))
 console.log(path.dirname(__filename))
+console.log(path.basename(__filename))
+console.log(path.extname(__filename))
+
+console.log(path.parse(__filename))
