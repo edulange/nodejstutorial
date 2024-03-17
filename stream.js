809c2368520a39a09ef6dcf3d216ec52762290de
Eduardo Lange <eduardoandrelange@gmail.com>
2024-03-17 14:41:49 -0300

    ok

--- stream.js
+++ stream.js
@@ -1,1 +1,15 @@
-https://youtu.be/f2EqECiTBL8?si=_7YDfOceB1Z1NUGy&t=2253
+const fs = require('fs')
+const path = require('path')
+//path.join(__dirname, 'files', 'lorem.txt') mesma coisa que ('./files/lorem.txt')
+
+
+const rs = fs.createReadStream(path.join(__dirname, 'files', 'lorem.txt'), {encoding: 'utf-8'})
+
+const ws = fs.createWriteStream('./files/new-lorem.txt')
+
+/* mesma coisa que o rs.pipe
+rs.on('data', (dataChunk) => {
+    ws.write(dataChunk)
+}) */
+
+rs.pipe(ws)
