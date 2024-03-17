49abfce5aaa20a51c4cca8d20640a5becc1a03cf
Eduardo Lange <eduardoandrelange@gmail.com>
2024-03-17 12:40:56 -0300

    file read

---
+++ index.js
@@ -0,0 +1,7 @@
+const fs = require('fs')
+
+fs.readFile('./files/starter.txt', 'utf-8', (err, data) => {
+	if (err) throw err
+	console.log('data', data)
+	console.log('data.toString()', data.toString())
+})
