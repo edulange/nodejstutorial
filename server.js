1f5af76432ea81fa54c96d08a171c07911f1f205
Eduardo Lange <eduardoandrelange@gmail.com>
2024-03-18 19:34:15 -0300

    creating a private server with logs

--- server.js
+++ server.js
@@ -6,19 +6,30 @@ const fsPromises = require('fs').promises
 const logEvents = require('./logEvents')
 const EventEmitter = require('events')
 class Emitter extends EventEmitter {}
-//initialize objets
+// initialize object
 const myEmitter = new Emitter()
-
-/* myEmitter.on('log', (msg) => logEvents(msg))
-myEmitter.emit('log', 'Log event emitted!') */
-
+myEmitter.on('log', (msg, fileName) => logEvents(msg, fileName))
 const PORT = process.env.PORT || 3500
 
-const server = http.createServer((req, res) => {
-	console.log('req.url :>> ', req.url)
-	console.log('req.method :>> ', req.method)
+const serveFile = async (filePath, contentType, response) => {
+	try {
+		const rawData = await fsPromises.readFile(filePath, !contentType.includes('image') ? 'utf8' : '')
+		const data = contentType === 'application/json' ? JSON.parse(rawData) : rawData
+		response.writeHead(filePath.includes('404.html') ? 404 : 200, { 'Content-Type': contentType })
+		response.end(contentType === 'application/json' ? JSON.stringify(data) : data)
+	} catch (err) {
+		console.log(err)
+		myEmitter.emit('log', `${err.name}: ${err.message}`, 'errLog.txt')
+		response.statusCode = 500
+		response.end()
+	}
+}
 
-	const extesion = path.extname(req.url)
+const server = http.createServer((req, res) => {
+	console.log(req.url, req.method)
+	myEmitter.emit('log', `${req.url}\t${req.method}`, 'reqLog.txt')
+
+	const extension = path.extname(req.url)
 
 	let contentType
 
@@ -44,7 +55,36 @@ const server = http.createServer((req, res) => {
 		default:
 			contentType = 'text/html'
 	}
-	https://youtu.be/f2EqECiTBL8?si=A4Em9yuNmAIbYRQY&t=5757
-})
 
+	let filePath =
+		contentType === 'text/html' && req.url === '/'
+			? path.join(__dirname, 'views', 'index.html')
+			: contentType === 'text/html' && req.url.slice(-1) === '/'
+			? path.join(__dirname, 'views', req.url, 'index.html')
+			: contentType === 'text/html'
+			? path.join(__dirname, 'views', req.url)
+			: path.join(__dirname, req.url)
+
+	// makes .html extension not required in the browser
+	if (!extension && req.url.slice(-1) !== '/') filePath += '.html'
+
+	const fileExists = fs.existsSync(filePath)
+
+	if (fileExists) {
+		serveFile(filePath, contentType, res)
+	} else {
+		switch (path.parse(filePath).base) {
+			case 'old-page.html':
+				res.writeHead(301, { Location: '/new-page.html' })
+				res.end()
+				break
+			case 'www-page.html':
+				res.writeHead(301, { Location: '/' })
+				res.end()
+				break
+			default:
+				serveFile(path.join(__dirname, 'views', '404.html'), 'text/html', res)
+		}
+	}
+})
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
