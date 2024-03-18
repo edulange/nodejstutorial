f9e9594e7e90fd69a1694fdf6c43cf5731a8e011
Eduardo Lange <eduardoandrelange@gmail.com>
2024-03-17 21:24:11 -0300

    parando por hoje.

--- server.js
+++ server.js
@@ -1,14 +1,50 @@
+const http = require('http')
+const path = require('path')
+const fs = require('fs')
+const fsPromises = require('fs').promises
+
 const logEvents = require('./logEvents')
 const EventEmitter = require('events')
-class MyEmitter extends EventEmitter {}
-
+class Emitter extends EventEmitter {}
 //initialize objets
-const myEmitter = new MyEmitter()
+const myEmitter = new Emitter()
 
-//add listener for the log event
-myEmitter.on('log', (msg) => logEvents(msg))
+/* myEmitter.on('log', (msg) => logEvents(msg))
+myEmitter.emit('log', 'Log event emitted!') */
 
-setTimeout(() => {
-	//Emit Event
-	myEmitter.emit('log', 'Log event emitted!')
-}, 2000)
+const PORT = process.env.PORT || 3500
+
+const server = http.createServer((req, res) => {
+	console.log('req.url :>> ', req.url)
+	console.log('req.method :>> ', req.method)
+
+	const extesion = path.extname(req.url)
+
+	let contentType
+
+	switch (extension) {
+		case '.css':
+			contentType = 'text/css'
+			break
+		case '.js':
+			contentType = 'text/javascript'
+			break
+		case '.json':
+			contentType = 'application/json'
+			break
+		case '.jpg':
+			contentType = 'image/jpeg'
+			break
+		case '.png':
+			contentType = 'image/png'
+			break
+		case '.txt':
+			contentType = 'text/plain'
+			break
+		default:
+			contentType = 'text/html'
+	}
+	https://youtu.be/f2EqECiTBL8?si=A4Em9yuNmAIbYRQY&t=5757
+})
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
