8a045a542848e13d6061992e06dbb9cac0a4ed16
Eduardo Lange <eduardoandrelange@gmail.com>
2024-03-28 20:22:55 -0300

    godzeira

--- controllers/employeesController.js
+++ controllers/employeesController.js
@@ -11,7 +11,7 @@ const getAllEmployees = (req, res) => {
 
 const createNewEmployee = (req, res) => {
 	const newEmployee = {
-        id: data.employees?.length ? data.employees[data.employees.length - 1].id + 1 : 1,
+		id: data.employees?.length ? data.employees[data.employees.length - 1].id + 1 : 1,
 		firstname: req.body.firstname,
 		lastname: req.body.lastname,
 	}
@@ -20,20 +20,31 @@ const createNewEmployee = (req, res) => {
 		return res.status(400).json({ message: 'First name and last names are required.' })
 	}
 
-    https://youtu.be/f2EqECiTBL8?si=WnLmdyrkcKPfgKGg&t=12967
-
-    data.setEmployees([...data.employees, newEmployee])
-    res.json(data.employees)
+	data.setEmployees([...data.employees, newEmployee])
+	res.status(201).json(data.employees)
 }
 
 const updateEmployee = (req, res) => {
-	res.json({
-		firstname: req.body.firstname,
-		lastname: req.body.lastname,
-	})
+	const employee = data.employees.find((emp) => emp.id === parseInt(req.body.id))
+	if (!employee) {
+		return res.status(400).json({ message: `Employee ID ${req.body.id} not found` })
+	}
+	if (req.body.firstname) employee.firstname = req.body.firstname
+	if (req.body.lastname) employee.lastname = req.body.lastname
+	const filteredArray = data.employees.filter((emp) => emp.id !== parseInt(req.body.id))
+	const unsortedArray = [...filteredArray, employee]
+	data.setEmployees(unsortedArray.sort((a, b) => (a.id > b.id ? 1 : a.id < b.id ? -1 : 0)))
+	res.json(data.employees)
 }
 
 const deleteEmployee = (req, res) => {
+	const employee = data.employees.find((emp) => emp.id === parseInt(req.body.id))
+	if (!employee) {
+		return res.status(400).json({ message: `Employee ID ${req.body.id} not found` })
+	}
+	
+	const filteredArray = data.employees.filter(emp => emp.id !== parseInt(req.body.id))
+	data.setEmployees([...filteredArray])
 	res.json({ id: req.body.id })
 }
 
