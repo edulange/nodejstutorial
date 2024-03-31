c4da60b0a402390827f7af7a09b1f3535de3079d
Eduardo Lange <eduardoandrelange@gmail.com>
2024-03-31 14:49:25 -0300

    wow

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const corsOptions = require('./config/corsOptions')
 const { logger } = require('./middleware/logEvents')
 const errorHandler = require('./middleware/errorHandler')
 const verifyJWT = require('./middleware/verifyJWT')
+const cookieParser = require('cookie-parser')
 const PORT = process.env.PORT || 3500
 
 //custom middleware logger // o logger precisa ficar cedo, pra começar a sempre dar log xD
@@ -20,6 +21,9 @@ app.use(express.urlencoded({ extended: false }))
 
 //built-in middleware for json
 app.use(express.json())
+
+//middleware for cookies
+app.use(cookieParser())
 
 //serve static files
 app.use(express.static(path.join(__dirname, '/public')))
