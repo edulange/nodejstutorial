6a4d534af5d3ebdefc0cd60181784f7c6830463c
Eduardo Lange <eduardoandrelange@gmail.com>
2024-04-05 22:33:22 -0300

    using mongo instead of local db

--- controllers/employeesController.js
+++ controllers/employeesController.js
@@ -1,10 +1,68 @@
-const data = {
-	employees: require('../model/employees.json'),
-	setEmployees: function (data) {
-		this.employees = data
-	},
+const Employee = require('../model/Employee')
+
+const getAllEmployees = async (req, res) => {
+	const employees = await Employee.find()
+	if (!employees) return res.status(204).json({ message: 'No employees found' })
+	res.json(employees)
 }
 
+const createNewEmployee = async (req, res) => {
+	if (!req?.body?.firstname || req?.body?.lastname) {
+		return res.status(400).json({ message: 'First name and last name required' })
+	}
+
+	try {
+		const result = await Employee.create({
+			firstname: req.body.firstname,
+			lastname: req.body.lastname,
+		})
+
+		res.status(201).json(result)
+	} catch (err) {
+		console.error(err)
+	}
+}
+
+const updateEmployee = async (req, res) => {
+	if (!req?.body?.id) return res.status(400).json({ message: 'ID parameter is required' })
+
+	const employee = await Employee.findOne({ _id: req.body.id }).exec()
+
+	if (!employee) return res.status(204).json({ message: `No employee ID matches id ${req.body.id}` })
+
+	if (req.body?.firstname) employee.firstname = req.body.firstname
+	if (req.body?.lastname) employee.lastname = req.body.lastname
+
+	const result = await employee.save()
+	res.json(result)
+}
+
+const deleteEmployee = async (req, res) => {
+	if (!req?.body?.id) return res.status(400).json({ message: 'ID parameter is required' })
+
+	const employee = await Employee.findOne({ _id: req.body.id }).exec()
+	if (!employee) return res.status(204).json({ message: `No employee ID matches id ${req.body.id}` })
+
+	const result = await Employee.deleteOne({ _id: req.body.id })
+	res.json(result)
+}
+
+const getEmployee = async (req, res) => {
+	if (!req?.params?.id) return res.status(400).json({ message: 'ID parameter is required' })
+	const employee = await Employee.findOne({ _id: req.body.id }).exec()
+	if (!employee) return res.status(204).json({ message: `No employee ID matches id ${req.params.id}` })
+
+	res.json(employee)
+}
+
+module.exports = {
+	getAllEmployees,
+	createNewEmployee,
+	updateEmployee,
+	deleteEmployee,
+	getEmployee,
+}
+/*  COMO QUE ERA ANTES COM O DATABASE LOCAL INVES DO MONGO
 const getAllEmployees = (req, res) => {
 	res.json(data.employees)
 }
@@ -54,12 +112,4 @@ const getEmployee = (req, res) => {
         return res.status(400).json({ "message": `Employee ID ${req.params.id} not found` });
     }
     res.json(employee);
-}
-
-module.exports = {
-	getAllEmployees,
-	createNewEmployee,
-	updateEmployee,
-	deleteEmployee,
-	getEmployee,
-}
+} */
